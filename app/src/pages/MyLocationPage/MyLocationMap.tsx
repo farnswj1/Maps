f1936265f30bcb82228032f36daee906f1fca45e
Justin Farnsworth <farnswj1@tcnj.edu>
2024-05-07 12:18:08 -0400

    enable high accuracy on geolocation API

--- app/src/pages/MyLocationPage/MyLocationMap.tsx
+++ app/src/pages/MyLocationPage/MyLocationMap.tsx
@@ -12,34 +12,38 @@ const MyLocationMap: FC = () => {
       const current = ref.current;
       const listeners: google.maps.MapsEventListener[] = [];
 
-      navigator.geolocation.getCurrentPosition(({ coords }) => {
-        const { latitude, longitude } = coords;
-        const position = { lat: latitude, lng: longitude };
-        const options: google.maps.MapOptions = {
-          mapId: "my-location-map",
-          center: position,
-          zoom: 16
-        };
-        const map = new google.maps.Map(current, options);
-        const manager = new MarkerManager(map, {});
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          const { latitude, longitude } = coords;
+          const position = { lat: latitude, lng: longitude };
+          const options: google.maps.MapOptions = {
+            mapId: "my-location-map",
+            center: position,
+            zoom: 16
+          };
+          const map = new google.maps.Map(current, options);
+          const manager = new MarkerManager(map, {});
 
-        google.maps.event.addListener(manager, 'loaded', async () => {
-          const { AdvancedMarkerElement } = (
-            await google.maps.importLibrary('marker') as google.maps.MarkerLibrary
-          );
-          const infoWindow = new google.maps.InfoWindow();
-          const marker = new AdvancedMarkerElement({ map, position });
+          google.maps.event.addListener(manager, 'loaded', async () => {
+            const { AdvancedMarkerElement } = (
+              await google.maps.importLibrary('marker') as google.maps.MarkerLibrary
+            );
+            const infoWindow = new google.maps.InfoWindow();
+            const marker = new AdvancedMarkerElement({ map, position });
 
-          const listener = marker.addListener('click', () => {
-            infoWindow.close();
-            infoWindow.setContent(renderToString(<Popover coords={coords} />));
-            infoWindow.open(marker.map, marker);
+            const listener = marker.addListener('click', () => {
+              infoWindow.close();
+              infoWindow.setContent(renderToString(<Popover coords={coords} />));
+              infoWindow.open(marker.map, marker);
+            });
+
+            listeners.push(listener);
+            manager.refresh();
           });
-
-          listeners.push(listener);
-          manager.refresh();
-        });
-      });
+        },
+        null,
+        { enableHighAccuracy: true }
+      );
 
       return () => listeners.forEach(listener => (
         google.maps.event.removeListener(listener)
